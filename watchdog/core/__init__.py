d2fe6847e2586ed06db97098e7ab15477f810304
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2023-03-15 11:18:16 -0300

    feat: implement late runs

--- watchdog/core/__init__.py
+++ watchdog/core/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from .executor import Executor  # noqa: F401
+from . import settings  # noqa: F401
--- watchdog/core/executor.py
+++ watchdog/core/executor.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 from typing import List
 
+from namesgenerator import get_random_name
+
 from watchdog.handlers.base import Handler
 from watchdog.triggers.base import Trigger
 from watchdog.utils import log
 
 
 class Executor:
-    def __init__(self, trigger: Trigger, handlers: List[Handler]):
+    def __init__(
+        self, trigger: Trigger, handlers: List[Handler], friendly_name: str = None
+    ):
         self._trigger = trigger
         self._handlers = handlers
-        log("Starting executor with:")
-        log("Trigger: {}".format(trigger.__class__.__name__))
-        log("Handlers: {}".format([h.__class__.__name__ for h in handlers]))
+        self._friendly_name = friendly_name or get_random_name()
+        log(f"Starting executor {self._friendly_name} with:")
+        log(" * Trigger: {}".format(trigger.__class__.__name__))
+        log(" * Handlers: {}".format([h.__class__.__name__ for h in handlers]))
 
     def run(self):
-        log("Executor running")
+        log(f"Executor {self._friendly_name} running")
         trigger, info = self._trigger.trigger()
         log("- Trigger: {}".format(trigger))
         if trigger:
